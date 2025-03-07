f95d2a1c190c04a070e6d17409b00831f93082a0
Timofii Solovey <116516370+SoloveyTimofey@users.noreply.github.com>
2025-03-07 17:50:31 +0200

    Update GetAllChartGroupsQuery.cs

--- server/DailyNotesChart/src/Core/DailyNotesChart.Application/Operations/ChartGroups/Queries/GetAllChartGroupsQuery.cs
+++ server/DailyNotesChart/src/Core/DailyNotesChart.Application/Operations/ChartGroups/Queries/GetAllChartGroupsQuery.cs
@@ -1,7 +1,6 @@
 ﻿using DailyNotesChart.Application.Abstractions.MediatrSpecific;
-using DailyNotesChart.Application.DTOs.ChartGroups;
-using DailyNotesChart.Domain.Models.ChartGroupAggregate.AggregateRoot;
+using DailyNotesChart.Application.ReadModels;
 
 namespace DailyNotesChart.Application.Operations.ChartGroups.Queries;
 
-public sealed record GetAllChartGroupsQuery() : IQuery<List<ChartGroupReadDto>>;
+public sealed record GetAllChartGroupsQuery() : IQuery<List<ChartGroupReadModel>>;
